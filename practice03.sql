b013ea088552bece6dd07041257c0d2a728e2286
오경환 <okyeonghwan5@gmail.com>
2021-03-18 09:05:41 +0900

    reboot test

--- practice03.sql
+++ practice03.sql
@@ -3,8 +3,7 @@
 직원들의 사번(employee_id), 이름(firt_name), 성(last_name)과 부서명(department_name)을
 조회하여 부서이름(department_name) 오름차순, 사번(employee_id) 내림차순 으로 정렬하세
 요.
-(106건)
-*/
+*/ 
 SELECT employee_id, 
     first_name, 
     last_name, 
